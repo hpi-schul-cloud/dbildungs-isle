a68b64d45f1f57738081899d41e73640797fda0d
Salima Hassouni <salima.hassouni@capgemini.com>
2024-04-28 20:19:01 +0000

    dbp-104-generate tls cert

--- charts/dbildungs-iam-server/gencert.sh
+++ charts/dbildungs-iam-server/gencert.sh
@@ -59,7 +59,7 @@ generate_cert redis "Generic-cert"
 
 NUM_NODES=6 
 for ((i=0; i<$NUM_NODES; i++)); do
-    generate_cert "tls_cluster/redis-$i" "Redis-$i" ""
+    generate_cert "tls/redis-$i" "Redis-$i" ""
 done
 
 [ -f tls/redis.dh ] || openssl dhparam -out tls/redis.dh 2048
