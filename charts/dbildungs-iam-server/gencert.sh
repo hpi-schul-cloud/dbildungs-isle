5cfd8c2d9b4c51e4ebaf70d6bb6c0b618921150b
Salima Hassouni <salima.hassouni@capgemini.com>
2024-05-04 14:14:57 +0000

    dbp-774 correct redis conf

--- charts/dbildungs-iam-server/gencert.sh
+++ charts/dbildungs-iam-server/gencert.sh
@@ -2,20 +2,22 @@
 
 # Generate some test certificates which are used by the regression test suite:
 #
-#   /tmp/tls/ca.{crt,key}          Self signed CA certificate.
-#   /tmp/tls/redis.{crt,key}       A certificate with no key usage/policy restrictions.
-#   /tmp/tls/client.{crt,key}      A certificate restricted for SSL client usage.
-#   /tmp/tls/server.{crt,key}      A certificate restricted for SSL server usage.
-#   /tmp/tls/redis.dh              DH Params file.
+#   tls/ca.{crt,key}          Self signed CA certificate.
+#   tls/redis.{crt,key}       A certificate with no key usage/policy restrictions.
+#   tls/client.{crt,key}      A certificate restricted for SSL client usage.
+#   tls/server.{crt,key}      A certificate restricted for SSL server usage.
+#   tls/redis.dh              DH Params file.
 
-CERT_DIR="/tmp/tls"
+CERT_DIR="tls"
 
 generate_cert() {
     local name=$1
     local cn="$2"
     local opts="$3"
+
     local keyfile=$CERT_DIR/${name}.key
     local certfile=$CERT_DIR/${name}.crt
+
     [ -f $keyfile ] || openssl genrsa -out $keyfile 2048
     openssl req \
         -new -sha256 \
@@ -31,6 +33,7 @@ generate_cert() {
             $opts \
             -out $certfile
 }
+
 mkdir -p $CERT_DIR
 [ -f $CERT_DIR/ca.key ] || openssl genrsa -out $CERT_DIR/ca.key 4096
 openssl req \
@@ -39,15 +42,19 @@ openssl req \
     -days 3650 \
     -subj '/O=Redis Test/CN=Certificate Authority' \
     -out $CERT_DIR/ca.crt
+
 cat > $CERT_DIR/openssl.cnf <<_END_
 [ server_cert ]
 keyUsage = digitalSignature, keyEncipherment
 nsCertType = server
+
 [ client_cert ]
 keyUsage = digitalSignature, keyEncipherment
 nsCertType = client
 _END_
+
 generate_cert server "Server-only" "-extfile $CERT_DIR/openssl.cnf -extensions server_cert"
 generate_cert client "Client-only" "-extfile $CERT_DIR/openssl.cnf -extensions client_cert"
 generate_cert redis "Generic-cert"
+
 [ -f $CERT_DIR/redis.dh ] || openssl dhparam -out $CERT_DIR/redis.dh 2048
